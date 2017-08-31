5a6238c3de7ab578597fb2bbab6aa37d0619683b
Chatoyant <me@nameless.studio>
2017-08-31 23:32:20 +0800

    1

--- ResNet/model.py
+++ ResNet/model.py
@@ -1,14 +1,21 @@
 import torch.nn as nn
+import torch.nn.functional as F
+
 
 class BasicBlock(nn.Module):
-    def __init(self):
+    def __init__(self):
         pass
-    
     def forward(self, x):
-
+        pass
 
 class Bottleneck(nn.Module):
-    pass
+    def __init__(self):
+        pass
+    def forward(self, x):
+        pass
 
-class ResNet(nn.Module):
-    pass
+def ResNet(nn.Module):
+    def __init__(self):
+        pass
+    def forward(self, x):
+        pass
