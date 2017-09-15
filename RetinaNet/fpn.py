265cceea3952c4f76f5143bbc5f407ec2fcc32a3
JesseYang <jesse.yang1985@gmail.com>
2017-09-15 18:03:40 +0800

    a

---
+++ RetinaNet/fpn.py
@@ -0,0 +1,15 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class Bottleneck(nn.Module):
+    def __init__(self, in_planes, planes, stride=1):
+        pass
+    def forward(self):
+        pass
+
+
+class FPN(nn.Module):
+    def __init__(self):
+        pass
+    def forward(self):
--- RetinaNet/model.py
+++ RetinaNet/model.py
@@ -3,4 +3,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 
-class
+class Net(nn.Module):
+    def __init__(self):
+        pass
+    def forward(self):
